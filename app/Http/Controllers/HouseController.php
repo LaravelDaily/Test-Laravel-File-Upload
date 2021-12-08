00c41a7f8a3b7956d2ba3560175dadab18aad46f
thinkverse <hallberg.kim@gmail.com>
2021-12-08 17:31:21 +0100

    Solve `test_update_file_remove_old_one` test

--- app/Http/Controllers/HouseController.php
+++ app/Http/Controllers/HouseController.php
@@ -24,7 +24,9 @@ class HouseController extends Controller
     {
         $filename = $request->file('photo')->store('houses');
 
-        // TASK: Delete the old file from the storage
+        if (Storage::exists($house->photo)) {
+            Storage::delete($house->photo);
+        }
 
         $house->update([
             'name' => $request->name,
