b00ef012c5882ffda312b8a35a71d9389ff50db6
parmartanu101996@gmail.com <Mannat@12345>
2021-12-09 14:22:50 +0530

    Task Completed

--- app/Http/Controllers/OfficeController.php
+++ app/Http/Controllers/OfficeController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Models\Office;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Storage;
 
 class OfficeController extends Controller
 {
@@ -14,6 +15,8 @@ class OfficeController extends Controller
         // TASK: Upload the file "photo" so it would be written as
         //   storage/app/public/offices/[original_filename]
 
+        $request->file('photo')->storeAs('public/offices/', $filename);
+//        Storage::disk('public')->put(offices/',  $request->file('photo'));
         Office::create([
             'name' => $request->name,
             'photo' => $filename,
