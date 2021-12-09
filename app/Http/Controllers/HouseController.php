dde5404892c12194b8b78d4b69b1c423e355cb74
Peter Bessenyei <peter.bessenyei@gmail.com>
2021-12-09 15:16:34 +0100

    test challenge

--- app/Http/Controllers/HouseController.php
+++ app/Http/Controllers/HouseController.php
@@ -23,8 +23,7 @@ class HouseController extends Controller
     public function update(Request $request, House $house)
     {
         $filename = $request->file('photo')->store('houses');
-
-        // TASK: Delete the old file from the storage
+        Storage::delete($house->photo);
 
         $house->update([
             'name' => $request->name,
@@ -36,7 +35,6 @@ class HouseController extends Controller
 
     public function download(House $house)
     {
-        // TASK: Return the $house->photo file from "storage/app/houses" folder
-        // for download in browser
+        return Storage::download($house->photo);
     }
 }
