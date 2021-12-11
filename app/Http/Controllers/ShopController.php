b21c734dcdb6afb46c7a93ec218d7aa1ab80ec7f
Justin Byrne <theskaterjesus@gmail.com>
2021-12-11 15:21:32 +0000

    Task 6: Added resize and save

--- app/Http/Controllers/ShopController.php
+++ app/Http/Controllers/ShopController.php
@@ -15,6 +15,8 @@ class ShopController extends Controller
         // TASK: resize the uploaded image from /storage/app/shops/$filename
         //   to size of 500x500 and store it as /storage/app/shops/resized-$filename
         // Use intervention/image package, it's already pre-installed for you
+        $img = Image::make($request->file('photo')->getRealPath());
+        $img->resize('500','500')->save(storage_path('shops/resized-' . $filename));
 
         return 'Success';
     }
