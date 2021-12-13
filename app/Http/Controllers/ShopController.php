dca65576aae2d646ef8ded77fe941f16ecf9403f
Leo Nuneza <leiyu876@yahoo.com>
2021-12-13 09:23:30 +0800

    Done

--- app/Http/Controllers/ShopController.php
+++ app/Http/Controllers/ShopController.php
@@ -15,6 +15,9 @@ class ShopController extends Controller
         // TASK: resize the uploaded image from /storage/app/shops/$filename
         //   to size of 500x500 and store it as /storage/app/shops/resized-$filename
         // Use intervention/image package, it's already pre-installed for you
+        Image::make($request->file('photo')->path())->resize(500, 500, function ($constraint) {
+            $constraint->aspectRatio();
+        })->save(storage_path().'/app/shops/resized-'.$filename);
 
         return 'Success';
     }
