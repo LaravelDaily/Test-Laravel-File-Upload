2caa38a6b3b9f4bcd325c63076f4c793479ba815
Mohamed Sorour <mas.qa77@gmail.com>
2022-01-30 21:28:25 +0200

    Fixed task 5 problem

--- app/Http/Controllers/OfficeController.php
+++ app/Http/Controllers/OfficeController.php
@@ -14,7 +14,7 @@ class OfficeController extends Controller
         // TASK: Upload the file "photo" so it would be written as
         //   storage/app/public/offices/[original_filename]
 
-        $request->file('photo')->storeAs('offices', $filename);
+        $request->file('photo')->storeAs('public/offices', $filename);
 
         Office::create([
             'name' => $request->name,
