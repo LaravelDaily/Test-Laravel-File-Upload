4dcd979ccda9d5cd2700a79588b8247cdd6a546a
Abdullah Hamdy <Obaida.hamdy.1998@gmail.com>
2022-03-08 11:35:59 +0200

    task 3

--- app/Http/Controllers/HouseController.php
+++ app/Http/Controllers/HouseController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Models\House;
+use Illuminate\Contracts\Cache\Store;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Storage;
 
@@ -22,9 +23,12 @@ class HouseController extends Controller
 
     public function update(Request $request, House $house)
     {
+
+
         $filename = $request->file('photo')->store('houses');
 
         // TASK: Delete the old file from the storage
+        Storage::delete($house->photo);
 
         $house->update([
             'name' => $request->name,
