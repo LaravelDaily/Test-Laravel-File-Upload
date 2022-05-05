40560d23566e45eb62410ff3e4172f6248fa0b3a
David Villalobos <davidvillalobosvargas@gmail.com>
2022-05-04 18:57:52 -0600

    task5

--- app/Http/Controllers/OfficeController.php
+++ app/Http/Controllers/OfficeController.php
@@ -13,6 +13,7 @@ class OfficeController extends Controller
 
         // TASK: Upload the file "photo" so it would be written as
         //   storage/app/public/offices/[original_filename]
+        $request->file('photo')->storeAs('offices','storage/app/public/offices/'.$filename);
 
         Office::create([
             'name' => $request->name,
