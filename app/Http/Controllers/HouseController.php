3e62f4246220371726b109c7eb5275c0d5c47810
David Villalobos <davidvillalobosvargas@gmail.com>
2022-05-04 18:46:11 -0600

    task3

--- app/Http/Controllers/HouseController.php
+++ app/Http/Controllers/HouseController.php
@@ -25,6 +25,7 @@ class HouseController extends Controller
         $filename = $request->file('photo')->store('houses');
 
         // TASK: Delete the old file from the storage
+        Storage::delete([$house->photo]);
 
         $house->update([
             'name' => $request->name,
