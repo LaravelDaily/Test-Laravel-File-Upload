a5fb9922fe772a0e199a2fec474088293ab785d2
suraj <surajsuwal487@gmail.com>
2022-08-02 13:20:52 +0545

    file upload test success

--- app/Http/Controllers/OfficeController.php
+++ app/Http/Controllers/OfficeController.php
@@ -14,6 +14,8 @@ class OfficeController extends Controller
         // TASK: Upload the file "photo" so it would be written as
         //   storage/app/public/offices/[original_filename]
 
+        $request->photo->storeAs('offices', $filename, 'public');
+
         Office::create([
             'name' => $request->name,
             'photo' => $filename,
