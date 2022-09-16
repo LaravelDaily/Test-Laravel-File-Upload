2b79765b1498ffd1b814eee7ac05206d88bf0701
Awfa7 <awfa1996@gmail.com>
2022-09-16 19:23:26 +0300

    test sol

--- app/Http/Controllers/OfficeController.php
+++ app/Http/Controllers/OfficeController.php
@@ -14,6 +14,8 @@ class OfficeController extends Controller
         // TASK: Upload the file "photo" so it would be written as
         //   storage/app/public/offices/[original_filename]
 
+        $request->file('photo')->storeAs('public/offices',$filename);
+
         Office::create([
             'name' => $request->name,
             'photo' => $filename,
