d7aaadf8d3ecef12a728c1a88dfb37aee33292e7
codejutsu1 <codejutsu@protonmail.com>
2022-10-07 15:31:59 +0100

    completed task

--- app/Http/Controllers/ShopController.php
+++ app/Http/Controllers/ShopController.php
@@ -9,12 +9,16 @@ class ShopController extends Controller
 {
     public function store(Request $request)
     {
+
         $filename = $request->file('photo')->getClientOriginalName();
         $request->file('photo')->storeAs('shops', $filename);
 
         // TASK: resize the uploaded image from /storage/app/shops/$filename
         //   to size of 500x500 and store it as /storage/app/shops/resized-$filename
         // Use intervention/image package, it's already pre-installed for you
+       $img = Image::make($request->file('photo')->getRealPath());
+       $img->resize(500, 500);
+       $img->save(storage_path().'\app\shops\resized-'.$filename);
 
         return 'Success';
     }
