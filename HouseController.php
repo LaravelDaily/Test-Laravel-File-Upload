162676ed5d35e47c378c598020e23363cb5bd4ef
Ilija <ilija.angelov@yahoo.com>
2022-12-14 21:13:10 +0100

    Add files via upload

---
+++ HouseController.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\House;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Storage;
+
+class HouseController extends Controller
+{
+    public function store(Request $request)
+    {
+        $filename = $request->file('photo')->store('houses');
+
+        House::create([
+            'name' => $request->name,
+            'photo' => $filename,
+        ]);
+
+        return 'Success';
+    }
+
+    public function update(Request $request, House $house)
+    {
+        $filename = $request->file('photo')->store('houses');
+
+        // TASK: Delete the old file from the storage
+        if(Storage::exists($house->photo)){
+            Storage::delete($house->photo);
+        }
+        $house->update([
+            'name' => $request->name,
+            'photo' => $filename,
+        ]);
+
+        return 'Success';
+    }
+
+    public function download(House $house)
+    {
+        // TASK: Return the $house->photo file from "storage/app/houses" folder
+        // for download in browser
+        return Storage::download($house->photo);
+    }
+}
---
+++ ShopController.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Intervention\Image\Facades\Image;
+
+class ShopController extends Controller
+{
+    public function store(Request $request)
+    {
+        $filename = $request->file('photo')->getClientOriginalName();
+        $request->file('photo')->storeAs('shops', $filename);
+
+        // TASK: resize the uploaded image from /storage/app/shops/$filename
+        //   to size of 500x500 and store it as /storage/app/shops/resized-$filename
+        // Use intervention/image package, it's already pre-installed for you
+        $img = Image::make($request->file('photo'))->resize(500, 500)->save('storage/app/shops/resized-'.$filename);
+
+        return 'Success';
+    }
+}
