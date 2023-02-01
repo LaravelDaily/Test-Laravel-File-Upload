1132977699c7f9d34a536f14ad843210489d4651
Alptekin <alptekin.ocakdan@gmail.com>
2023-02-01 15:50:18 +0300

    Tasks Completed

--- app/Http/Controllers/ShopController.php
+++ app/Http/Controllers/ShopController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Storage;
 use Intervention\Image\Facades\Image;
 
 class ShopController extends Controller
@@ -10,11 +11,13 @@ class ShopController extends Controller
     public function store(Request $request)
     {
         $filename = $request->file('photo')->getClientOriginalName();
-        $request->file('photo')->storeAs('shops', $filename);
+        $path = $request->file('photo')->storeAs('shops', $filename);
 
         // TASK: resize the uploaded image from /storage/app/shops/$filename
         //   to size of 500x500 and store it as /storage/app/shops/resized-$filename
         // Use intervention/image package, it's already pre-installed for you
+        $photo = Image::make(Storage::get($path))->resize(500, 500);
+        $photo->save(Storage::path('shops').'/resized-' .$filename);
 
         return 'Success';
     }
