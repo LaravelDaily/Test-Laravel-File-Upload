4db1cc224cf0cdb8cf5d7f16340b086777df9ec8
gofran04 <docreader04@gmail.com>
2023-03-15 22:17:29 +0200

    fileupload 1

--- app/Http/Controllers/ShopController.php
+++ app/Http/Controllers/ShopController.php
@@ -10,12 +10,18 @@ class ShopController extends Controller
     public function store(Request $request)
     {
         $filename = $request->file('photo')->getClientOriginalName();
-        $request->file('photo')->storeAs('shops', $filename);
+        $path = $request->file('photo')->storeAs('shops', $filename);
 
         // TASK: resize the uploaded image from /storage/app/shops/$filename
         //   to size of 500x500 and store it as /storage/app/shops/resized-$filename
         // Use intervention/image package, it's already pre-installed for you
+        $img = Image::make("storage/app/shops/{$filename}");
+        $img->resize(500, 500);
+        $img->save("storage/app/shops/resized-{$filename}");
 
+        // $img = Image::make(storage_path("/app/shops/{$filename}"));
+        // $img->resize(500, 500);
+        // $img->save(storage_path("/app/shops/resized-{$filename}"));
         return 'Success';
     }
 }
