6bb0e8fd491ec86679eb1c38501ef29b288d986f
inam925 <97148236+inam925@users.noreply.github.com>
2023-04-16 09:04:07 +0500

    Update ShopController.php

--- app/Http/Controllers/ShopController.php
+++ app/Http/Controllers/ShopController.php
@@ -15,9 +15,9 @@ class ShopController extends Controller
         // TASK: resize the uploaded image from /storage/app/shops/$filename
         //   to size of 500x500 and store it as /storage/app/shops/resized-$filename
         // Use intervention/image package, it's already pre-installed for you
-        $image = Image::make('/storage/app/shops/' . $filename)
+        $image = Image::make(storage_path('app/shops' . $filename))
             ->resize(500, 500)
-            ->storeAs('shops/resized-' . $filename, $filename);
+            ->save(storage_path('app/shops/resized-' . $fielname));
 
         return 'Success';
     }
