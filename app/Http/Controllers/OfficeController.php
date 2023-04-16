824a37bc52970fef0dbb85ceff4031b97145fb77
inam925 <97148236+inam925@users.noreply.github.com>
2023-04-16 08:38:49 +0500

    Update OfficeController.php

--- app/Http/Controllers/OfficeController.php
+++ app/Http/Controllers/OfficeController.php
@@ -13,7 +13,7 @@ class OfficeController extends Controller
 
         // TASK: Upload the file "photo" so it would be written as
         //   storage/app/public/offices/[original_filename]
-        $request->file('photo')->storeAs('storage/app/public/offices', $filename);
+        $request->file('photo')->storeAs('storage/app/public/offices', $filename, 'public');
 
         Office::create([
             'name' => $request->name,
