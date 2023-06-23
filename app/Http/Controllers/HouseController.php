49e5485a808db7a42f537159a20980bf46ec011d
Hector <hmreluz@gmail.com>
2023-06-23 00:32:29 -0500

    Test update file remove old one

--- app/Http/Controllers/HouseController.php
+++ app/Http/Controllers/HouseController.php
@@ -24,7 +24,7 @@ class HouseController extends Controller
     {
         $filename = $request->file('photo')->store('houses');
 
-        // TASK: Delete the old file from the storage
+        Storage::delete($house->photo);
 
         $house->update([
             'name' => $request->name,
