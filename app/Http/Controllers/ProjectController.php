5cd96992318b28c2728fbdd04175e9a72797ad27
mhammad-bazzarah <mhamadbazzarah2@gmail.com>
2023-09-13 22:13:38 +0300

    solving file_upload_test

--- app/Http/Controllers/ProjectController.php
+++ app/Http/Controllers/ProjectController.php
@@ -5,17 +5,19 @@ namespace App\Http\Controllers;
 use App\Models\Project;
 use Illuminate\Http\Request;
 
+
 class ProjectController extends Controller
 {
     public function store(Request $request)
     {
         $request->validate([
             // TASK: Write the validation rule so "logo" file would be MAX 1 megabyte
+            'logo' => 'file|size:1024'
         ]);
 
         // TASK: change the below line so that $filename would contain only filename
         // The same filename as the original uploaded file
-        $filename = '???';
+        $filename = $request->file('logo')->getClientOriginalName();
         $request->file('logo')->storeAs('logos', $filename);
 
         Project::create([
