b571d1fbf1bc070338f25ef441b990c6aa23656d
kareem-hub <kareemehab968@gmail.com>
2023-11-18 12:52:36 +0200

    7 tests passed

--- app/Http/Controllers/HouseController.php
+++ app/Http/Controllers/HouseController.php
@@ -24,7 +24,7 @@ class HouseController extends Controller
     {
         $filename = $request->file('photo')->store('houses');
 
-        // TASK: Delete the old file from the storage
+        Storage::delete($house->photo);
 
         $house->update([
             'name' => $request->name,
@@ -38,5 +38,6 @@ class HouseController extends Controller
     {
         // TASK: Return the $house->photo file from "storage/app/houses" folder
         // for download in browser
+        return Storage::download($house->photo);
     }
 }
