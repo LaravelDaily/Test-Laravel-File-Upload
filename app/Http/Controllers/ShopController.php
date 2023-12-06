f1e385d80d738bb8e8a2a92f4a5ce03e34f9472e
Marko Milicevic <marko.milicevic@digitalinsurance.io>
2023-12-06 23:27:03 +0100

    solve tests

--- app/Http/Controllers/ShopController.php
+++ app/Http/Controllers/ShopController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Storage;
 use Intervention\Image\Facades\Image;
 
 class ShopController extends Controller
@@ -15,6 +16,9 @@ class ShopController extends Controller
         // TASK: resize the uploaded image from /storage/app/shops/$filename
         //   to size of 500x500 and store it as /storage/app/shops/resized-$filename
         // Use intervention/image package, it's already pre-installed for you
+        $img = Image::make(Storage::get('shops'.'/'.$filename));
+        $img->resize(500,500);
+        $img->save(Storage::path('shops'). '/' . 'resized-'.$filename);
 
         return 'Success';
     }
