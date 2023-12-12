af7a48c95360f008fa2f335140e8d9db76fa87d0
bilawal shaikh <46257281+bilawalshaikh@users.noreply.github.com>
2023-12-12 18:40:00 +0500

    Update OfficeController.php

--- app/Http/Controllers/OfficeController.php
+++ app/Http/Controllers/OfficeController.php
@@ -14,6 +14,12 @@ class OfficeController extends Controller
         // TASK: Upload the file "photo" so it would be written as
         //   storage/app/public/offices/[original_filename]
 
+        if (!file_exists(storage_path('app/public'). '/' . 'offices')) {
+            Storage::disk('public')->makeDirectory('/offices');
+        }
+        Storage::disk('public')->put('offices' . '/' . $filename , $request->file('photo'));
+        
+
         Office::create([
             'name' => $request->name,
             'photo' => $filename,
