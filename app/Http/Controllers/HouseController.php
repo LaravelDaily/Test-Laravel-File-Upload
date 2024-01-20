5385165c1a8cd009e1ca1dd42a5704b3d741c5c2
OhmygoodR <109280933+OhmygoodR@users.noreply.github.com>
2024-01-20 12:01:28 +0800

    Update HouseController.php

--- app/Http/Controllers/HouseController.php
+++ app/Http/Controllers/HouseController.php
@@ -25,7 +25,7 @@ class HouseController extends Controller
         $filename = $request->file('photo')->store('houses');
 
         // TASK: Delete the old file from the storage
-        Storage::delete($request->file('photo'));
+        Storage::delete($request->photo->path());
 
         $house->update([
             'name' => $request->name,
