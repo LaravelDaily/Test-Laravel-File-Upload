1d0a4f0b4115608033038636415a928875a1b069
OhmygoodR <109280933+OhmygoodR@users.noreply.github.com>
2024-01-29 22:57:59 +0800

    Update ProjectController.php

--- app/Http/Controllers/ProjectController.php
+++ app/Http/Controllers/ProjectController.php
@@ -16,7 +16,7 @@ class ProjectController extends Controller
         // TASK: change the below line so that $filename would contain only filename
         // The same filename as the original uploaded file
         // $filename = $request->file('logo')->getClientOriginalName();
-        $filename='loga';
+        $filename=$request->file('logo')->getClientOriginalExtension();
         $request->file('logo')->storeAs('logos', $filename);
 
         Project::create([
