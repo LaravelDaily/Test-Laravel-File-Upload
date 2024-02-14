8fb81fdf576e19d31d3e1662947ac1c9616ee975
OhmygoodR <109280933+OhmygoodR@users.noreply.github.com>
2024-02-14 22:07:33 +0800

    Update CompanyController.php

--- app/Http/Controllers/CompanyController.php
+++ app/Http/Controllers/CompanyController.php
@@ -20,7 +20,7 @@ class CompanyController extends Controller
     public function show(Company $company)
     {
         // TASK: retrieve the full URL to the uploaded photo file, using Spatie Media Library
-        $photo = $company[0]->getFullUrl();
+        $photo = $company->getMedia('photo')[0]->getFullUrl();
 
         return view('companies.show', compact('company', 'photo'));
     }
